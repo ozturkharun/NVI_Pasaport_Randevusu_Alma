10bfb0377242411188c6437213404b9f16580ca5
ozturkharun <harun.ozturk.6212@gmail.com>
2022-12-26 14:25:02 +0300

    Hata mesajları, while loop ve try-exceptler düzenlendi, sayaç düzeltildi

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
 import time
 
 print("Randevu alma modülüne hoşgeldiniz.")
@@ -13,9 +16,11 @@ tel="546..."
 
 # --------Driver Kurulumu--------
 
-browser_path = "C:\Drivers\chromedriver.exe"
-driver = webdriver.Chrome(executable_path=browser_path)
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options, service=Service(ChromeDriverManager().install()))
 driver.get("https://randevu.nvi.gov.tr/")
+
 
 # --------Web sitesi içindeki xpath ler (baş)--------
 
@@ -39,7 +44,6 @@ il_path =       '/html/body/div[1]/form/section/div/div[2]/div[3]/div/div/a[2]/s
 
 # step3 () Sayfası
 
-
 # --------Web sitesi içindeki xpath ler (son)--------
 
 
@@ -53,6 +57,7 @@ time.sleep(2)
 driver.find_element("xpath", bordo_pasaport_path).click()
 time.sleep(1)
 print("Bordo pasaport seçildi!")
+
 # Bilgilerin Doldurulması
 driver.find_element("xpath", isim_path).send_keys(isim)
 driver.find_element("xpath", soyisim_path).send_keys(soyisim)
@@ -74,18 +79,19 @@ print("Güvenlik kodu girildi, işleme devam ediliyor...")
 # Seçilen İlin Belirlenen İlçelerinde Boşluk Kontrolü
 ilce_listesi = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
                 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41]
-tarih_listesi = ["27.12.2022" , "28.12.2022" , "24.12.2022"]
+tarih_listesi = ["26.12.2022"]
 yenileme_sayisi = 0
 
-while True:
-    # !!!---Aşağıdaki xpath il_path ile değişiecek---!!!
+sonuc = False
+while not sonuc:
+    # !!!---Aşağıdaki xpath kullanıcı seçimine göre belirlenecekilek---!!!
     driver.find_element("xpath", il_path).click()
     time.sleep(1)
     print("İstenen şehre tıklandı!")
+    hata_sebebi="Seçilen ilçelerde uygun bir randevu bulunamadı!"
 
     for i in ilce_listesi:
-        # Bu döngü while loop için True ya da False üretmeli
-
+        # Boş randevu tarihi varsa try komutu gerçekleşir
         try:
             # İlçede randevu var demek
             driver.find_element("xpath",'/html/body/div[1]/form/section/div/div[2]/div[5]/div/div/a[{0}]/div/div/label'.format(i))
@@ -106,7 +112,6 @@ while True:
 
                 try:
                     # Randevu saati hala varsa
-
                     #Randevu saati kontrolü:
                     driver.find_element("xpath","/html/body/div[1]/form/section/div[1]/div[2]/div[3]/div[1]/div[2]/div[2]/div/div/div")
                     #Randevu saati çekme:
@@ -120,39 +125,29 @@ while True:
                     time.sleep(0.5)
                     # !!!---Telefonla doğrulama aşamasına gelmemek için aşağıdaki kod deaktif edilir
                     #driver.find_element("xpath","/html/body/div[1]/form/section/div[1]/div[2]/div[3]/div[2]/div/div[2]/button").click()
-
                     sonuc = True
                     break
+
                 except:
                     # Randevu saati kaybolmuşsa
-                    print("Randevu saati seçilemedi, İşlemler tekrarlanacak!")
+                    hata_sebebi ="Hata sebebi = Randevu saati seçilemedi, işlemler tekrarlanacak!"
                     driver.get("https://randevu.nvi.gov.tr/default/step2")
-                    sonuc = False
-                    # burada ne olacak?
+                    break
+                    # For looptan çıkacak
+
             else:
                 # Randevu günü talep edilen gün ile uyuşmuyorsa
-                print(uygun_tarih, "istenen tarihler içerisinde yok.")
-                raise Exception
+                print("İlçe", i, "içerisindeki uygun tarih (", uygun_tarih, ") talep edilen tarihler arasında yok!")
+
 
         except:
-            # İlçede randevu yok demek
-            hata_sebebi = "Seçilen ilçelerde uygun bir randevu bulunamadı!"
-            sonuc = False
+            # İlçede randevu yok veya alınamadı demek
+            pass
 
-    if sonuc == True:
-        print(uygun_tarih,"tarihindeki saat", randevu_saati, "randevusunu almak için telefonunuza gelen sms onay kodunu girin!")
+    if sonuc==True:
+        print(uygun_tarih, "tarihindeki saat", randevu_saati, "randevusunu almak için telefonunuza gelen sms onay kodunu girin!")
         break
 
     else:
-        # İlçede randevu yok, while loop ile yenileyip devam et
-        print(hata_sebebi)
         yenileme_sayisi += 1
-        print("Randevu alınamadı, yeniden deneniyor! Yenilenme sayısı:", yenileme_sayisi)
-
-
-
-
-"""
-tercih edilen ilçeler seçilecek üsküdar-beykoz vb
-tercih edilen tarihler verilecek
-"""
+        print(hata_sebebi, "Randevu alma işlemi tekrarlanıyor... Yenilenme sayısı:", yenileme_sayisi)
